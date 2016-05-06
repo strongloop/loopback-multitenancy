94aa83724ee1d116c35e28ed84f6f8dd512d1ea2
Simon Ho <simonho@ca.ibm.com>
2016-05-05 17:27:18 -0700

    Add tenant resolver

--- test/init.js
+++ test/init.js
@@ -1,3 +1,10 @@
 var chai = require('chai');
+var dirtyChai = require('dirty-chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+chai.use(dirtyChai);
+chai.use(sinonChai);
 
 global.expect = chai.expect;
+global.sinon = sinon;
--- test/smoke.js
+++
@@ -1,5 +0,0 @@
-describe('smoke', function() {
-  it('should pass', function() {
-    expect(1).to.equal(1);
-  });
-});
