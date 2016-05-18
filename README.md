035811ba209e96a80df095674c218db4573936e6
Simon Ho <simonho@ca.ibm.com>
2016-05-18 16:28:50 -0700

    Add model resolver

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # loopback-multitenancy
 
-Multitenancy components for LoopBack.
+Multitenancy middleware for LoopBack.
 
 ## Tenant resolver middleware
 
@@ -15,4 +15,30 @@ app.use('/api/:tenantId/:modelId/:modelName', tenantResolver);
 //req.tenant.id = :tenantId (String)
 //req.tenant.modelId = :modelId (String)
 //req.tenant.modelName = :modelName (String)
+```
+
+*Takes `/api/1/Joe/Todo` and creates req.tenant = {id: '1' modelId: 'Joe',
+modelName: 'Todo'}*
+
+## Model resolver middleware
+
+Transforms the URL path to a namespaced URL path using the tenant data provided
+in the request object (`req.tenant`).
+
+*Changes `/api/1/Joe/Todo` to `/api/1_Joe_Todo*
+
+## Usage
+
+```
+var modelResolver = require('loopback-multitenancy').modelResolver;
+app.use(app.get('restApiRoot'), modelResolver);
+app.use(app.get('restApiRoot'), loopback.rest());
+
+//a request to /api/1/Joe/Todo comes in
+//
+//the tenant resolver massages the URL to a tenant object:
+//req.tenant = {id: '1', modelId: 'Joe', modelName: Todo};
+//
+//the model resolver sets modifies the endpoint based on the tenant data
+//req.url = '/api/1_Joe_Todo';
 ```
---
+++ test/integration/model-resolver.js
@@ -0,0 +1,53 @@
+var expect = require('../init').expect;
+var loopback = require('loopback');
+var modelResolver = require('../../lib/model-resolver');
+var request = require('supertest');
+var tenantResolver = require('../../lib/tenant-resolver');
+
+var app, Todo;
+
+describe('model resolver', function() {
+  beforeEach(setUpLoopBackAppWithResolvers);
+
+  it('maps the URL to a namespaced URL', function(done) {
+    var observedUrl;
+    app.use(function(req, res, next) {
+      observedUrl = req.url;
+      next();
+    });
+    app.use(app.get('restApiRoot'), loopback.rest());
+    request(app).get('/api/1/Joe/Todos').end(function(err, res) {
+      if (err) return done(err);
+      expect(observedUrl).to.equal('/api/1_Joe_Todos');
+      done();
+    });
+  });
+
+  it('returns the correct model data', function(done) {
+    app.use(app.get('restApiRoot'), loopback.rest());
+    Todo.create({content: 'a'}, function(err, todo) {
+      if (err) return done(err);
+
+      request(app).get('/api/1/Joe/Todos').expect(200, function(err, res) {
+        if (err) return done(err);
+        expect(res.body).to.eql([{id: 1, content: 'a'}]);
+        done();
+      });
+    });
+  });
+});
+
+function setUpLoopBackAppWithResolvers() {
+  app = loopback({localRegistry: true});
+
+  var db = app.dataSource('db', {connector: 'memory'});
+  Todo = app.registry.createModel('1_Joe_Todo');
+  Todo.attachTo(db);
+  app.model(Todo);
+
+  app.set('legacyExplorer', false);
+  app.set('restApiRoot', '/api');
+
+  app.use('/api/:tenantId/:modelId/:modelName', tenantResolver);
+  app.use(modelResolver);
+}
